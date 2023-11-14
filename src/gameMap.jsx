85bd0ccd683d0f99f13e1b199886ae5bef3b884d
afhorton <106110920+afhorton@users.noreply.github.com>
2023-11-14 11:52:55 -0700

    Here

--- src/gameMap.jsx
+++ src/gameMap.jsx
@@ -1,4 +1,4 @@
-import React [ useState, useEffect ] from 'react';
+import React, { useState, useEffect } from 'react';
 
 function GameMap () {
     const X = {walk: false, action: false,};
@@ -8,7 +8,7 @@ function GameMap () {
 
     const FIELD_MAP = [
     // X:0  1  2  3  4  5  6  7  8
-        [X, X, X, X, X, X, X, X, X], // y = 0
+        [O, O, O, X, X, X, X, X, X], // y = 0
         [X, O, O, O, O, O, O, O, X], // y = 1
         [X, O, O, O, O, O, O, O, X], // y = 2
         [X, O, O, O, O, O, O, O, X], // y = 3
@@ -20,37 +20,104 @@ function GameMap () {
     ]
 
     // useState for the player's current position
-    const [playerPosition, setPlayerPosition] = useState({x: 1, y: 1}); 
+    const [playerPosition, setPlayerPosition] = useState({top: 32, left: 32}); 
 
-    // Handle keydown events for player's movement
-    const handleKeyDown = (event) => {
-        switch(event.key) {
-            case 'ArrowUp':
-                if (FIELD_MAP[playerPosition.y - 1][playerPosition.x].walk) {
-                    setPlayerPosition({x: playerPosition.x, y: playerPosition.y - 1});
-                }
-                break;
-            case 'ArrowDown':
-                if (FIELD_MAP[playerPosition.y + 1][playerPosition.x].walk) {
-                    setPlayerPosition({x: playerPosition.x, y: playerPosition.y + 1});
-                }
-                break;
-            case 'ArrowLeft':
-                if (FIELD_MAP[playerPosition.y][playerPosition.x - 1].walk) {
-                    setPlayerPosition({x: playerPosition.x - 1, y: playerPosition.y});
-                }
-                break;
-            case 'ArrowRight':
-                if (FIELD_MAP[playerPosition.y][playerPosition.x + 1].walk) {
-                    setPlayerPosition({x: playerPosition.x + 1, y: playerPosition.y});
-                }
-                break;
-            default:
-                break;
-        }
+    // handle keypress events for player movement
+    const handleKeyPress = (event) => {
+        let newPosition;
+        let X;
+        let Y;
+    
+
+    switch (event.key) {
+        case 'ArrowUp':
+            newPosition = playerPosition.top - 32;
+            X = (playerPosition.left - (playerPosition.left % 32)) / 32;
+            Y = (newPosition - (newPosition % 32)) / 32;
+
+            if (FIELD_MAP[Y][X].walk) {
+                setPlayerPosition((prevPosition) => ({...prevPosition, top: newPosition}));
+            }
+            break;
+
+        case 'ArrowDown':
+            newPosition = playerPosition.top + 32;
+            X = (playerPosition.left - (playerPosition.left % 32)) / 32;
+            Y = (newPosition - (newPosition % 32)) / 32;
+
+            if (FIELD_MAP[Y][X].walk) {
+                setPlayerPosition((prevPosition) => ({...prevPosition, top: newPosition}));
+            }
+            break;
+        
+        case 'ArrowLeft':
+            newPosition = playerPosition.left - 32; 
+            X = ((playerPosition.left - 32) - ((playerPosition.left - 32) % 32)) / 32;
+            Y = (playerPosition.top - (playerPosition.top % 32)) / 32;
+
+            if (FIELD_MAP[Y][X].walk) {
+                setPlayerPosition((prevPosition) => ({...prevPosition, left: newPosition}));
+            }
+            break;
+
+        case 'ArrowRight':
+            newPosition = playerPosition.left + 32; 
+            X = ((playerPosition.left + 32) - ((playerPosition.left + 32) % 32)) / 32;
+            Y = (playerPosition.top - (playerPosition.top % 32)) / 32;
+
+            if (FIELD_MAP[Y][X].walk) {
+                setPlayerPosition((prevPosition) => ({...prevPosition, left: newPosition}));
+            }
+            break;
+
+        default:
+            break;
+    }
+
     };
 
-    return (
+    React.useEffect(
+        () => {
+            window.addEventListener('keydown', handleKeyPress);
+            return () => {
+                window.removeEventListener('keydown', handleKeyPress);
+            };
+        }, []
+    );
 
-    )
+    return (
+        <div>
+            {FIELD_MAP.map(
+                (row, rowIndex) =>
+                (
+                <div key={rowIndex}>
+                    {row.map((tile, colIndex) =>
+                    (
+                <div
+              key={colIndex}
+              style={{
+                width: 32,
+                height: 32,
+                backgroundColor: tile.walk ? 'green' : 'red',
+                display: 'inline-block',
+              }}
+            />
+                    ))}
+                </div>
+       ))}
+      <div
+        style={{
+          position: 'absolute',
+          top: playerPosition.top,
+          left: playerPosition.left,
+          width: 32,
+          height: 32,
+          backgroundColor: 'blue',
+        }}
+      />
+    </div>
+  );
 }
+
+
+export default GameMap;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import GameMap from './gameMap'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GameMap />
   </React.StrictMode>,
 )
